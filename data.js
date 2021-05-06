f1367665303f8a321a7dfe37d74870ef42013b4b
devdibs <ben.dibley@hotmail.com>
2021-05-06 16:09:22 +0200

    up to date

--- data.js
+++ data.js
@@ -1,31 +1,15 @@
 const key = 'LfQdp7cUG9XAPs8J2nSkfmViMYGeegjU';
 
 
-const getWeather = async (id) => {
+
     const baseUrl = 'http://dataservice.accuweather.com/currentconditions/v1/';
 
-    const query = `${id}?apikey=${key}`;
-
-    const response = await fetch(baseUrl + query);
-
-    const data = await response.json();
-
-    return (data[0]);
-  
-};
+    
 
 
 
 
-const getCity =  async (city) => {
+
     const baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
 
-    const query = `?apikey=${key}&q=${city}`;
-
-    const response = await fetch(baseUrl + query);
-
-    const data = await response.json();
-
-     return (data[0]);
-};
-
+   
