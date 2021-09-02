a5a197fab04a5c4810dfb0836eba398290616efc
devdibs <ben.dibley@hotmail.com>
2021-09-02 08:03:13 +0200

    first weather app commit

--- app.js
+++ app.js
@@ -3,32 +3,33 @@ const details = document.querySelector('.details');
 const background = document.querySelector('body');
 
 
+
 const updateUI = (data) => {
 
-    const cityDeets = data.cityDeets;
-    const weather = data.weather;
-
-    if(weather.IsDayTime == false) {
+   const { cityDeets, weather } = data;
+    
+     if(weather.IsDayTime == false) {
         background.style='background-color:0B1026';
-    } else {
+      } else {
         background.style='background-color:#87ceeb';
-    }
+      }
 
 
-    details.innerHTML = `<div class="details">
-    <div class="date date-style">
-        <h3>${cityDeets.EnglishName}, ${cityDeets.Country.ID}</h3>
-    </div>
-    <div class="weather weather-style">
-    <img class="icon-style" src="icons/${weather.WeatherIcon}.svg" alt="">
-        <h4>${weather.WeatherText}</h4>
-    </div>
-    <div class="temp temp-style">
-      <h4><span>${weather.Temperature.Metric.Value}</span>
-        <span>&deg;C</span></h4>
-    </div>
-   </div>`
-}
+        details.innerHTML = `
+                <div class="details">
+                <div class="date date-style">
+                    <h3>${cityDeets.EnglishName}, ${cityDeets.Country.ID}</h3>
+                </div>
+                <div class="weather weather-style">
+                    <img class="icon-style" src="icons/${weather.WeatherIcon}.svg" alt="">
+                    <h4>${weather.WeatherText}</h4>
+                </div>
+                <div class="temp temp-style">
+                    <h4><span>${weather.Temperature.Metric.Value}</span>
+                    <span>&deg;C</span></h4>
+                </div>
+                </div>`
+        }
 
 const updateCity = async (city) => {
 
--- data.js
+++ data.js
@@ -1,37 +1,25 @@
-const key = '2h5CaSiodwsjbpHf5Eiv1ZFzgNkAH3ed';
+const key = 'IALa6aNhNzj2AcQZFIGPGrQzdoKxpqfK';
 
 const getWeather = async (id) => {
 
     const baseUrl = 'http://dataservice.accuweather.com/currentconditions/v1/';
-
     const query = `${id}?apikey=${key}`;
-
     const response = await fetch(baseUrl + query);
-
     const data = await response.json();
     
     return data[0];
 }
 
 
-
-
 const getCity = async (city) => {
 
     const baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-
     const query = `?apikey=${key}&q=${city}`;
-
     const response = await fetch( baseUrl + query);
-
     const data = await response.json();
 
     return data[0];
 
 }
-// getCity('london').then(data => {
-//     return getWeather(data.Key);
-// }).then(data => {
-//     console.log(data);
-// }).catch(err => console.log(err));
+
 
